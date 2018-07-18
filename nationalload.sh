42c858cac75253d05c116d2c11028b53e9be0b30
karen <kkwentus@chef.io>
2018-07-18 12:57:10 -0500

    docker compose for development channel

--- nationalload.sh
+++ nationalload.sh
@@ -1,2 +1,2 @@
-echo ">>>>> hab svc load kkwentus/national-parks --bind database:np-mongodb.default --channel development --strategy at-once"
+echo ">>>>> hab svc load kkwentus/national-parks --bind database:np-mongodb.default --channel testing --strategy at-once"
 hab svc load kkwentus/national-parks --bind database:np-mongodb.default --channel development --strategy at-once 
